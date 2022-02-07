50bfa4e2f609cbbca8534e28ed204eb92e8e63da
Danniel C <dnncervantes.yahoo.com>
2022-02-06 20:04:37 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var cookieDiv = document.querySelector(".cookie-policy");
+function accept() {
+    cookieDiv.remove();
+}
+function loading() {
+    alert("Loading weather report..")
+}
+function c2f(temp) {
+    return Math.round(9 / 5 * temp + 32);
+}
+function f2c(temp) {
+    return Math.round(5 / 9 * (temp - 32));
+}
+function newCity(element) {
+    console.log(element.value);
+    for(var i=1; i<9; i++) {
+        var tempSpan = document.querySelector("#temp" + i);
+        var tempVal = parseInt(tempSpan.innerText);
+        if(element.value == "°C") {
+            tempSpan.innerText = f2c(tempVal);
+        } else {
+            tempSpan.innerText = c2f(tempVal);
+        }
+    }
+}
